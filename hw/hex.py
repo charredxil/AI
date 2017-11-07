ac693beb1b06c13a17350601d253643bc1a253a7
Charlie Gunn <cjgunnmail@gmail.com>
2017-11-06 21:43:31 -0500

    fix hex

--- hw/hex.py
+++ hw/hex.py
@@ -1,3 +1,4 @@
+import time
 unit = [{0, 1, 2, 6, 7, 8}, {2, 3, 4, 8, 9, 10}, {5, 6, 7, 12, 13, 14}, {7, 8, 9, 14, 15, 16}, {9, 10, 11, 16, 17, 18}, {13, 14, 15, 19, 20, 21}, {15, 16, 17, 21, 22, 23}]
 unitsof = [{0}, {0}, {0, 1}, {1}, {1}, {2}, {0, 2}, {0, 2, 3}, {0, 1, 3}, {1, 3, 4}, {1, 4}, {4}, {2}, {2, 5}, {2, 3, 5}, {3, 5, 6}, {3, 4, 6}, {4, 6}, {4}, {5}, {5}, {5, 6}, {6}, {6}]
 row = [set(range(0, 5)), set(range(5, 12)), set(range(12, 19)), set(range(19, 24)), {3, 4, 10, 11, 18}, {1, 2, 8, 9, 16, 17, 23}, {0, 6, 7, 14, 15, 21, 22}, {5, 12, 13, 19, 20}, {1, 0, 6, 5, 12}, {3, 2, 8, 7, 13, 14, 19}, {4, 10, 9, 16, 15, 21, 20}, {11, 18, 17, 23, 22}]
@@ -33,7 +34,9 @@ def solve(opts, solution, row_constr):
     opts[tofill] = cellopts
     return False
 
+start = time.time()
 row_constr = True
 opts  = {x : set(range(1, 8 if row_constr else 7)) for x in range(24)}
 solution = ['.' for _ in range(24)]
 solve(opts, solution, row_constr)
+print(time.time()-start)
---
+++ hw/hexbf.py
@@ -0,0 +1,28 @@
+units = [{0, 1, 2, 6, 7, 8}, {2, 3, 4, 8, 9, 10}, {5, 6, 7, 12, 13, 14}, {7, 8, 9, 14, 15, 16}, {9, 10, 11, 16, 17, 18}, {13, 14, 15, 19, 20, 21}, {15, 16, 17, 21, 22, 23}]
+rows = [{0, 1, 2, 3, 4}, {5, 6, 7, 8, 9, 10, 11}, {12, 13, 14, 15, 16, 17, 18}, {19, 20, 21, 22, 23}, {3, 4, 10, 11, 18}, {1, 2, 8, 9, 16, 17, 23}, {0, 6, 7, 14, 15, 21, 22}, {5, 12, 13, 19, 20}, {1, 0, 6, 5, 12}, {3, 2, 8, 7, 13, 14, 19}, {4, 10, 9, 16, 15, 21, 20}, {11, 18, 17, 23, 22}]
+
+row_constr = False #set to False for Q1, True for Q2
+groups = rows + units if row_constr else units
+def bruteForce(pzl, empty):
+    if isInvalid(pzl): return None
+    if not empty: return pzl
+    tofill = next(iter(empty))
+    empty.remove(tofill)
+    for num in range(1, 8 if row_constr else 7):
+        pzl[tofill] = str(num)
+        print(''.join(pzl))
+        bF = bruteForce(pzl, empty)
+        if bF: return bF
+    pzl[tofill] = '.'
+    empty.add(tofill)
+def isInvalid(pzl):
+    for g in groups:
+        seen = set()
+        for cell in g:
+            if pzl[cell] in seen: return True
+            if pzl[cell] != '.': seen.add(pzl[cell])
+    return False
+
+empty = set(range(24))
+pzl = ['.' for _ in range(24)]
+print(bruteForce(pzl, empty))
